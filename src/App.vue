63639df9a43cb411da9a5aa013703f0d27a5b067
Malakmilad90 <mmilad@beyond-creation.net>
2023-01-12 16:47:35 +0200

    first

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <signupformVue />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import signupformVue from './components/signupform.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    signupformVue
   }
 }
 </script>
@@ -22,5 +21,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body{
+  margin: 0;
+  background: #eee;
 }
 </style>
